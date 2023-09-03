e9e4283cdcce79c2d8ddf290fc15e32cdaa1a622
Daniel010203 <135991466+Daniel010203@users.noreply.github.com>
2023-09-03 16:02:00 -0300

    Update README.md

---
+++ Formação Python Developer - DIO/Desafio Básico Formação Python Developer.py
@@ -0,0 +1,5 @@
+T = int(str(input()))
+if  1 >= T <= 140:
+    print("TWEET")
+else:
+    print("MUTE")
---
+++ Formação Python Developer - DIO/Manipulando strings com python/Strings multiplas linhas.py
@@ -0,0 +1,7 @@
+nome = "Daniel"
+
+mensagem = f""" Olá meu nome é {nome},
+   Eu estou aprendendo Python. 
+   Essa mensagem possui diferentes recuos."""
+
+print(mensagem)
