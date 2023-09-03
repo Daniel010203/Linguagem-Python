0b505ddb98b0f42ae018391bec666ce3d6e7495c
Daniel010203 <135991466+Daniel010203@users.noreply.github.com>
2023-09-03 16:02:00 -0300

    Update README.md Add `node_ids` and `output_keys` as additional, finer grained filters for the `{upstream, downstream}_artifacts()` resolver functions.
    PiperOrigin-RevId: 569309679

--- Formação Python Developer - DIO/Desafio Intermediário Formação Python Developer - 2.py
+++ Formação Python Developer - DIO/Desafio Intermediário Formação Python Developer - 2.py
@@ -1,16 +1,12 @@
-# Função para calcular o número de garrafas no segundo dia
-def calcular_garrafas_segundo_dia(N, K):
-    garrafas = N  # Inicialmente, o cliente tem N garrafas
-
-    while N >= K:
-        trocas = N // K  # Calcula o número de trocas possíveis
-        garrafas += trocas  # Adiciona as garrafas obtidas nas trocas
-        N = trocas + (N % K)  # Atualiza o número de garrafas vazias
-
-    return garrafas
-
-# Leitura do número de casos de teste
 T = int(input())
+while T > 0:
+    T -= 1
+    N, K = input().split()
+    N = int(N)
+    K = int(K)
+    total = int(int(N % K) + int(N / K))
+
+    print(total)
 
 # Processamento e saída para cada caso de teste
 for _ in range(T):
---
+++ Inteligência Artificial e Machine Learning/O Teorema de Bayes é uma ferramenta fundamental na inferência estatística.py
@@ -0,0 +1,24 @@
+#O Teorema de Bayes, nomeado após o estatístico e matemático britânico Thomas Bayes, é uma ferramenta fundamental na teoria da probabilidade e na estatística. Ele desempenha um papel crucial na atualização de crenças ou probabilidades em face de novas evidências. O teorema é expresso da seguinte forma:
+
+#\[P(A|B) = \frac{P(B|A) \cdot P(A)}{P(B)}\]
+
+#Aqui estão algumas explicações e utilidades do Teorema de Bayes:
+
+#1. **Atualização de Crenças**: O Teorema de Bayes permite atualizar uma crença inicial (P(A)) com base em novas evidências (P(B|A)) para obter uma nova crença (P(A|B)). Ele é usado para incorporar informações adicionais em uma análise estatística, tornando-a especialmente útil quando se lida com inferências em ambientes incertos.
+
+#2. **Classificação e Reconhecimento de Padrões**: Na aprendizagem de máquina, o Teorema de Bayes é amplamente utilizado em classificação de padrões e tarefas de reconhecimento, como o classificador Naive Bayes. Ele permite calcular a probabilidade de uma classe (A) dado um conjunto de recursos (B).
+
+#3. **Diagnóstico Médico**: O Teorema de Bayes é usado em diagnóstico médico para calcular a probabilidade de um paciente ter uma determinada doença com base em resultados de testes e fatores de risco. Ele ajuda os médicos a tomar decisões informadas.
+
+#4. **Detecção de Spam**: Em sistemas de detecção de spam de e-mail, o Teorema de Bayes é usado para calcular a probabilidade de um e-mail ser spam com base em palavras-chave e padrões. Isso ajuda a classificar os e-mails como spam ou legítimos.
+
+#5. **Análise de Sentimento**: Em análise de sentimentos, o Teorema de Bayes é usado para determinar a probabilidade de um texto expressar sentimentos positivos ou negativos. Isso é amplamente utilizado em análise de redes sociais e mineração de opiniões.
+
+#6. **Previsão de Eventos Futuros**: O Teorema de Bayes é útil na previsão de eventos futuros com base em dados históricos e em informações atualizadas. Ele é usado em previsões de séries temporais, previsões meteorológicas e muitas outras aplicações.
+
+#7. **Tomada de Decisões em Ambientes Incertos**: Em situações onde a incerteza é alta, o Teorema de Bayes ajuda a tomar decisões informadas ao atualizar constantemente nossas crenças com base em novas evidências.
+
+#8. **Segurança Cibernética**: É usado na detecção de ataques cibernéticos, onde padrões de tráfego de rede e comportamento são avaliados para determinar se uma atividade é suspeita ou maliciosa.
+
+#Em resumo, o Teorema de Bayes é uma ferramenta poderosa para modelar e atualizar crenças e probabilidades com base em informações disponíveis. Sua versatilidade o torna uma ferramenta valiosa em uma ampla gama de aplicações, ajudando a tomar decisões mais informadas em ambientes incertos.
+
