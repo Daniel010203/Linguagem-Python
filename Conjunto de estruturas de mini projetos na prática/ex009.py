c94d88b72e839b20ec797d1dddae9debb87d2b01
Daniel de Oliveira Vieira <135991466+Daniel010203@users.noreply.github.com>
2023-11-22 20:32:09 -0300

    Update and rename ex009.py to 09 - Algoritmo que recebe um numero e mostra na tela sua tabuada

--- Conjunto de estruturas de mini projetos na prática/ex009.py
+++
@@ -1,5 +0,0 @@
-# Faça um programa que leia na tela um número qualquer e mostre na tela a sua tabuada.
-n = int(input("Digite um número para ver sua tabuuada: "))
-print("Tabuada de",n,"\\n")
-for i in range(1,11):
-    print(n,"x",i,"=",n*1)
