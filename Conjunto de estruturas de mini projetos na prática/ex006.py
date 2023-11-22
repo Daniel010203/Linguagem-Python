7dec1793a85cebb54d4f34321723e6d7a54efbb8
Daniel de Oliveira Vieira <135991466+Daniel010203@users.noreply.github.com>
2023-11-22 20:53:42 -0300

    Update and rename ex006.py to Algoritmo que leia um número e mostre seu sobro,triplo e a raiz quadrada na tela.

--- Conjunto de estruturas de mini projetos na prática/ex006.py
+++
@@ -1,7 +0,0 @@
-# Crie um algoritmo que leia um número e mostre seu sobro,triplo e a raiz quadrada na tela.
-# (\n) faz com que quebre a pagina.
-n = int(input("Digite um número: "))
-nd = n * 2
-nt = n * 3
-nq = n **(1/2)
-print(print("O número", n, " apresenta o ", nd, "como o dobro \n ", nt, "como o triplo e o \n", nq, "como a raiz quadrada"))
