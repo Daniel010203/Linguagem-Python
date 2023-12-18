167dfa748d86c2b560f54218cf8f4ddff0c4c0f5
Daniel de Oliveira Vieira <135991466+Daniel010203@users.noreply.github.com>
2023-12-18 14:27:44 -0300

    Algoritmo que leia a largura e a altura de uma parede em metros,calcule sua área e a quantidade de tinta necessária para pinta-la.Cada litro de tinta pinta apenas 2m².

--- Conjunto de estruturas de mini projetos na prática/ex011.py
+++ Conjunto de estruturas de mini projetos na prática/ex011.py
@@ -5,3 +5,17 @@ area = l * a
 print("Sua parede tem a dimensão de {}x{} e sua área é de {}m²".format(l,a,area))
 t = (l*a)/2
 print("Para pintar essa parede,você precisará de {}l de tinta.".format(t))
+
+Outra forma:
+litros = float(input("Qual a largura da parede que vamos pintar ? "))
+altura = float(input("Qual a altura da parede? "))
+area = litros * altura
+print("Se a parede tem a dimensão de {}x{} e sua área é de {}m²".format(litros,altura,area))
+tinta = (litros * altura)/2
+print("Paara pintar essa parede, será preciso {} litros de tinta.".format(tinta))
+
+
+Esse código solicita ao usuário a largura e altura de uma parede que será pintada. Em seguida, calcula a área da parede multiplicando a largura pela altura. 
+Depois, imprime a dimensão da parede e sua área em metros quadrados. 
+Em seguida, calcula a quantidade de tinta necessária para pintar a parede, dividindo a área por 2. 
+Por fim, imprime a quantidade de litros de tinta necessária.
