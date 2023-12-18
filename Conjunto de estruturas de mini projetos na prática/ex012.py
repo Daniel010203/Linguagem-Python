66f14d5df2692252fc550b26e1dfc9d56e5f714d
Daniel de Oliveira Vieira <135991466+Daniel010203@users.noreply.github.com>
2023-12-18 14:55:29 -0300

    Algoritmo que Código que mostre o preço de um produto e o desconto que pode ser aplicado ao mesmo.

--- Conjunto de estruturas de mini projetos na prática/ex012.py
+++ Conjunto de estruturas de mini projetos na prática/ex012.py
@@ -2,3 +2,21 @@
 preço = float(input("Digite o preço do produto:R$  "))
 novo = preço - (preço * 5 / 100)
 print("O produto que custava R${:.2f}, na promoção com desconto de 5%,vai custar R${:.2f}".format(preço,novo))
+
+outra forma:
+
+preco = float(input('Digite um valor para ver seu desconto em 5% : '))
+desconto = preco - ((preco*5) /100)
+
+print(f'''Esse valor com 5% de desconto fica em: 
+	\033[1;m{desconto} \033[m''')
+
+Outra forma:
+
+#Algoritmo que mostre o preço de um produto,calcule %5 de desconto e imprima na tela o novo valor:
+valorProduto = float(input("Digite o valor do produto: "))
+novoValorProduto =  valorProduto - (valorProduto * 5/100)
+print(" O valor do produto com os %5 de desconto derá de R${:.2f}.".format(novoValorProduto))
+Esse código solicita ao usuário que digite o valor de um produto. Em seguida, converte esse valor para um número decimal (float) usando a função `float()`. 
+Depois, o código calcula o valor do desconto aplicando uma taxa de 5% sobre o valor do produto. Isso é feito multiplicando o valor do produto por 5/100.
+Por fim, o código imprime na tela o valor do produto com o desconto aplicado, formatando-o para exibir apenas duas casas decimais usando a função `format()`.
