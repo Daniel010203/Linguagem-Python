93aa96d6af08922cb42cad6e6f6b6a1193f98a49
Daniel de Oliveira Vieira <135991466+Daniel010203@users.noreply.github.com>
2024-03-05 22:18:02 -0300

    Update 17_len.py

--- Formação Python Developer - DIO/Trabalhando com listas em Python/17_len.py
+++ Formação Python Developer - DIO/Trabalhando com listas em Python/17_len.py
@@ -1,3 +1,20 @@
 linguagens = ["python", "js", "c", "java", "csharp"]
 
 print(len(linguagens))  # 5
+
+Explicação da função em listas:
+Claro, vamos lá:
+
+```python
+linguagens = ["python", "js", "c", "java", "csharp"]
+```
+
+Neste trecho de código, criamos uma lista chamada "linguagens" que contém cinco elementos: "python", "js", "c", "java" e "csharp". Esta lista contém uma coleção ordenada de strings, representando linguagens de programação.
+
+```python
+print(len(linguagens))  # 5
+```
+
+Aqui, estamos utilizando a função embutida `len()` do Python para determinar o comprimento (número de elementos) da lista "linguagens". O comprimento é o número total de elementos na lista. Como a lista contém cinco elementos, a função `len()` retorna o valor 5.
+
+Então, quando executamos este código, ele imprimirá o número 5, que é o comprimento da lista "linguagens".
